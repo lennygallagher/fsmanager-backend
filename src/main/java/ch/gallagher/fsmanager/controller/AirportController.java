6097726f9dc4888a4d0018ab3e46eb307ccf7d7f
Leonhard Gallagher <lenny.gallagher@gmail.com>
2020-10-07 00:34:17 +0200

    add cors

--- src/main/java/ch/gallagher/fsmanager/controller/AirportController.java
+++ src/main/java/ch/gallagher/fsmanager/controller/AirportController.java
@@ -3,6 +3,7 @@ package ch.gallagher.fsmanager.controller;
 import ch.gallagher.fsmanager.persistence.Airport;
 import ch.gallagher.fsmanager.repository.AirportRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.transaction.Transactional;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/airports")
